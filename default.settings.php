2cbd38547fd8ca231474dd0392f9aad781682a37
Chin Godawita <chinthaka.godawita@demonzmedia.com>
2014-01-27 19:45:59 +1100

    Update default.settings.php
    Added description of datastore config.

--- default.settings.php
+++ default.settings.php
@@ -15,4 +15,5 @@ $conf['auth_pass'] = null;
 // Session timeout (in seconds).
 $conf['auth_timeout'] = 3600;
 
+// Where data should be stored. This will be created if it doesn't exist.
 $conf['datastore_path'] = $_SERVER['HOME'] . '/rest_exercise_data';
